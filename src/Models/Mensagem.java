7eb9e29c18cf659a700f636983018dbf1b4a4966
Alvaro Rodrigues <alvarordgs98@gmail.com>
2023-08-18 19:00:06 -0300

    criacao de algumas models

---
+++ src/Models/Mensagem.java
@@ -0,0 +1,12 @@
+package Models;
+
+import java.util.Date;
+
+public class Mensagem {
+    int id;
+    String pessoaOrigem;
+    String pessoaDestino;
+    String mensagem;
+    Date dataCriacao;
+    Date dataModificacao;
+}
---
+++ src/Models/Post.java
@@ -0,0 +1,11 @@
+package Models;
+
+import java.util.Date;
+
+public class Post {
+    int id;
+    String pessoa;
+    String conteudoDaMensagem;
+    Date dataCriacao;
+    Date dataModificacao;
+}
