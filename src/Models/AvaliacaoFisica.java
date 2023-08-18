4c9c1cb8b895545fe761f5a619c44ad906d76625
Ramon Dias Monteiro Silva <122894193+ramon541@users.noreply.github.com>
2023-08-18 19:15:40 -0300

    Adicionado algumas models

--- src/Models/AvaliacaoFisica.java
+++ src/Models/AvaliacaoFisica.java
@@ -4,6 +4,117 @@
  */
 package Models;
 
+import java.util.Date;
+
 public class AvaliacaoFisica {
-    
+    private int id;
+    private String pessoa;
+    private double peso;
+    private double altura;
+    private int idade;
+    private double pescoco;
+    private double cintura;
+    private double quadril;
+    private double massaGorda;
+    private double massaMagra;
+    private  Date dataCriacao;
+    private Date dataModificacao;
+
+    // criar métodos -> IMC, TMB, BF, body fat em kg, lean muscle em kg
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(String pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPescoco() {
+        return pescoco;
+    }
+
+    public void setPescoco(double pescoco) {
+        this.pescoco = pescoco;
+    }
+
+    public double getCintura() {
+        return cintura;
+    }
+
+    public void setCintura(double cintura) {
+        this.cintura = cintura;
+    }
+
+    public double getQuadril() {
+        return quadril;
+    }
+
+    public void setQuadril(double quadril) {
+        this.quadril = quadril;
+    }
+
+    public double getMassaGorda() {
+        return massaGorda;
+    }
+
+    public void setMassaGorda(double massaGorda) {
+        this.massaGorda = massaGorda;
+    }
+
+    public double getMassaMagra() {
+        return massaMagra;
+    }
+
+    public void setMassaMagra(double massaMagra) {
+        this.massaMagra = massaMagra;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(Date dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
 }
