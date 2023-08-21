341c9f48e7dace7971ab453bcb18b31e0ee1ae5c
Ramon Dias Monteiro Silva <122894193+ramon541@users.noreply.github.com>
2023-08-21 18:19:07 -0300

    Populando as classes restantes

---
+++ src/Models/TipoDieta.java
@@ -0,0 +1,69 @@
+package Models;
+
+import java.util.Date;
+
+public class TipoDieta {
+    private int id;
+    private String nome;
+    private double carboidrato;
+    private double proteina;
+    private double gordura;
+    private Date dataCriacao;
+    private Date dataModificacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getCarboidrato() {
+        return carboidrato;
+    }
+
+    public void setCarboidrato(double carboidrato) {
+        this.carboidrato = carboidrato;
+    }
+
+    public double getProteina() {
+        return proteina;
+    }
+
+    public void setProteina(double proteina) {
+        this.proteina = proteina;
+    }
+
+    public double getGordura() {
+        return gordura;
+    }
+
+    public void setGordura(double gordura) {
+        this.gordura = gordura;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(Date dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
+}
