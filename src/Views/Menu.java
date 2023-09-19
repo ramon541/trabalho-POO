6f1e80b5add6e3f0e3ae23008ae1c8d922e543f7
ramon541 <ramondiasmonteiro@gmail.com>
2023-09-19 11:48:30 -0300

    Adicionei a parte do menu principal e a funcionalidade logout + fechar programa

--- src/Views/Menu.java
+++ src/Views/Menu.java
@@ -27,6 +27,27 @@ public class Menu {
         return Integer.parseInt(scan.nextLine());
     }
 
+    public int menuPrincipal() {
+        StringBuilder builder = new StringBuilder("");
+
+        builder.append("==============================\n");
+        builder.append("SEJA BEM VINDO AO MEU PROGRAMA\n");
+        builder.append("==============================\n");
+        builder.append("\n1 - Alimento/Receita;");
+        builder.append("\n2 - Avaliação física;");
+        //No Registo de Dieta terá o Tipo de Dieta (Equilibrada, Low Carb, Cetogênica...) e também perguntará o Objetivo (Diminuir o Peso, Manter o Peso, Melhorar Composição Corporal e Aumentar o Peso.)
+        builder.append("\n3 - Registro de Dieta;");
+        //Em Refeições (café da manhã, almoço, café da tarde, janta, ceia ou outro nome) terá o Alimento das Refeições que iremos conseguri adicionar os elementos e terá também a Preferência
+        builder.append("\n4 - Refeições;");
+        builder.append("\n5 - Logout;");
+        builder.append("\n9 - Fechar programa;");
+        builder.append("\n\nQual a sua opção? R: ");
+
+        System.out.print(builder);
+
+        return Integer.parseInt(scan.nextLine());
+    }
+
     public Pessoa login() {
         String login;
         String senha;
@@ -41,7 +62,7 @@ public class Menu {
         return pessoaDAO.buscaLogin(login, senha);
     }
 
-    public void registrar (){
+    public void registrar () {
 
     }
 }
--- src/Views/View.java
+++ src/Views/View.java
@@ -6,6 +6,48 @@ import Models.Util;
 import java.util.Scanner;
 
 public class View {
+
+    public static int loopPrograma() {
+        Menu menu = new Menu();
+        Scanner scan = new Scanner(System.in);
+        int fecharPrograma = 0;
+        int opc = 0;
+        while (opc != 5) {
+            opc = menu.menuPrincipal();
+            switch (opc) {
+                case 1:
+                    System.out.println("Menu Alimento Refeição");
+                    break;
+
+                case 2:
+                    System.out.println("Menu Avaliação física");
+                    break;
+
+                case 3:
+                    System.out.println("Menu Registro de Dieta");
+                    break;
+
+                case 4:
+                    System.out.println("Menu Refeições");
+                    break;
+
+                case 5:
+                    System.out.println("Logout...");
+                    Util.setPessoaLogada(null);
+                    break;
+
+                case 9:
+                    opc = 5;
+                    fecharPrograma = 9;
+                    break;
+
+                default:
+                    System.out.println("\n\nOPÇÃO INVÁLIDA!!");
+                    break;
+            }
+        }
+        return fecharPrograma;
+    }
     public static void main(String[] args) {
         Menu menu = new Menu();
         Scanner scan = new Scanner(System.in);
@@ -20,7 +62,12 @@ public class View {
                         System.out.println("Login feito com sucesso!!");
                         Util.setPessoaLogada(logado);
 
-                        //Método do menu principal
+                        opc = loopPrograma();
+                        if (opc == 9){
+                            System.out.println("Saindo...");
+                            scan.close();
+                            System.exit(0);
+                        }
                     } else {
                         System.out.println("Login Inválido. Tente novamente...");
                     }
@@ -32,6 +79,7 @@ public class View {
 
                 default:
                     System.out.println("\n\nOPÇÃO INVÁLIDA!!");
+                    break;
             }
         }
     }
